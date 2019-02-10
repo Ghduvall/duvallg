dd6051e454d1673c4d84a454b08885990ad79a78
Grant Duvall <grant@mark4tech.com>
2019-02-10 00:11:00 -0600

    Added Assignment 2
    Got to Part 2 Video # 5 in tutorial.

---
+++ Assignment 2/Assets/Scripts/DataController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class DataController : MonoBehaviour {
+
+    private RoundData[] allRoundData;
+
+    private PlayerProgress playerProgress;
+    private string gamDataFileName = "data.json";
+
+	
+	void Start () {
+
+        DontDestroyOnLoad (gameObject);
+        LoadGameData();
+        LoadPlayerProgress();
+
+        SceneManager.LoadScene("MenuScreen");
+		
+	}
+
+    public RoundData GetCurrentRoundData()
+    {
+        return allRoundData [0];
+    }
+
+    public void SubmitNewPlayerScore(int newScore)
+    {
+        if (newScore > playerProgress.highestScore)
+        {
+            playerProgress.highestScore = newScore;
+            SavePlayerProgress();
+        }
+    }
+
+    public int GetHighestPlayerScore()
+    {
+        return playerProgress.highestScore;
+    }
+	
+    private void LoadPlayerProgress()
+    {
+        playerProgress = new PlayerProgress();
+
+        if (PlayerPrefs.HasKey("highestScore"))
+        {
+            playerProgress.highestScore = PlayerPrefs.GetInt("highestScore");
+        }
+    }
+
+    private void SavePlayerProgress()
+    {
+        PlayerPrefs.SetInt("highestScore", playerProgress.highestScore);
+    }
+
+
+
+    private void LoadGameData()
+    {
+        string filePath = Path.Combine(Application.streamingAssetsPath, gamDataFileName);
+
+        if (File.Exists(filePath))
+        {
+            string dataAsJson = File.ReadAllText(filePath);
+            GameData loadedData = JsonUtility.FromJson<GameData>(dataAsJson);
+
+            allRoundData = loadedData.allRoundData;
+        }
+        else
+        {
+            Debug.LogError("Cannot load game data!");
+        }
+    }
+
+
+}
---
+++ Quiz_Game/Assets/Question.cs
@@ -0,0 +1,13 @@
+﻿
+
+ [System.Serializable]
+public class Question {
+
+    public string fact;
+    public bool isTrue;
+
+
+
+
+
+}
