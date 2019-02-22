64f8a0803710c4560d1f4bce94383d1b88467ac0
Grant Duvall <grant@mark4tech.com>
2019-02-22 00:47:29 -0600

    Tutorial @23:30
    Got through 1/3 of Bracky's tutorial. Commiting for version control

---
+++ Assignment 3/Assets/Scripts/WordGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordGenerator : MonoBehaviour {
+
+    private static string[] wordList = { "word1", "word2", "word3","test1", "test2", "test3"};
+
+	public static string GetRandomWord ()
+    {
+        int randomIndex = Random.Range(0, wordList.Length);
+        string randomWord = wordList[randomIndex];
+
+        return randomWord;
+    }
+
+}
