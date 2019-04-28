d461670c8bfeed6ff937687f490b2cd5a1539984
Grant Duvall <grant@mark4tech.com>
2019-04-27 21:04:26 -0500

    Fruit Ninja Working (0)
    Basis of working game.

---
+++ Fruit Ninja Replica/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public Button introContinueBtn;
+    public Button optionsBtn;
+    public Button saveOptionsBtn;
+    public Button startBtn;
+    public Button restartBtn;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        introContinueBtn.onClick.AddListener(scenePreScore);
+
+        optionsBtn.onClick.AddListener(sceneOptions);
+
+        saveOptionsBtn.onClick.AddListener(sceneIntro);
+
+        startBtn.onClick.AddListener(sceneMain);
+
+        restartBtn.onClick.AddListener(sceneIntro);
+
+
+    }
+
+    public void sceneIntro()
+    {
+        GameScore.playerScore = 0;
+        SceneManager.LoadScene(0);
+    }
+    public void sceneOptions()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void scenePreScore()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void sceneMain()
+    {
+        SceneManager.LoadScene(3);
+    }
+    
+}
---
+++ Fruit Ninja Replica/Assets/Scripts/GameScore.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameScore : MonoBehaviour
+{
+    public ChangeScene changeScene;
+
+    public static int playerScore = 0;
+    public Text scoreText;
+    public static string score = "Score: ";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = score + playerScore.ToString();
+        
+        if (playerScore == 10)
+        {
+            SceneManager.LoadScene(4);
+        }
+    }
+}
